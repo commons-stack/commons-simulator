2bc71ae0fbfb91f2d45b276ae8bb5e4defd7ce1c
Andrew Chiw <randomshinichi4869@gmail.com>
2020-09-22 18:49:13 +0200

    convictionvoting_test.py actually test something

--- simulation/convictionvoting_test.py
+++ simulation/convictionvoting_test.py
@@ -4,5 +4,7 @@ from convictionvoting import trigger_threshold
 
 class ConvictionThresholdTest(unittest.TestCase):
     def test_small_proposal_should_have_low_threshold(self):
-        threshold = trigger_threshold(10, 1000, 10000000)
-        print(threshold)
+        threshold = trigger_threshold(10, 1000, 10000000, 0.2)
+
+        # This number is not special, just used to make sure everything stays the same
+        self.assertEqual(threshold, 5540166.20498615)
